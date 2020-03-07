f8b4969f0fa84cae5853c86bc74aaf5b99fe2caf
Douglas Hewitt <madebydouglas@gmail.com>
2020-03-06 17:09:12 -0800

    partial completion of create a mac app
    on step 9

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Douglas Hewitt on 3/5/20.
+//  Copyright © 2020 Douglas Hewitt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
